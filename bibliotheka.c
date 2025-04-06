0594a88064356c4e687321fe27e8f809d277cab2
Henrique França <henriquefrancasmm@gmail.com>
2025-04-06 16:40:37 -0300

    Corpo principal do programa.

---
+++ bibliotheka.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+double stockunits,stockstorage;
+
+/*
+Stocksunit = Unidades de estoque (a ser adicionada ou removida), Stock storage = Armazenamento de estoque. 
+
+
+*/
+
+int main(){
+
+
+
+
+    
+    
+    
+    return 0;
+}
