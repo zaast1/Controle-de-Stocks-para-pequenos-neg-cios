408b6be5df3561c9dd3b5260165e0b8bdbcfbcdb
surufel <henriquefrancasmm@gmail.com>
2025-05-24 22:12:53 -0300

    V0.1.0 - Programa feito com sucesso.

--- bibliotheka.c
+++ bibliotheka.c
@@ -2,20 +2,28 @@
 #include <stdlib.h>
 #include <locale.h>
 #include <string.h>
+#include <stdbool.h>
 
 FILE *ListaEstoques;
 
-
 int opMenu,opLerArquivo,opSimul; // Operações
-int stockunits, stockstorage, codigo; // stockunits = unidade operativa para alterar o stockstorage, stockstorage = armazenamento
-char name[24],description[200],expdate[10],buffer[1024];
+int stockunits; // stockunits = unidade operativa para alterar o stockstorage, stockstorage = armazenamento
+char buffer[1024];
 // Buffer é uma região de memória. Vai ser usado pra ler todo o arquivo.
-int ArquivoLista();
+
+typedef struct {
+    int stockstorage;
+    int codigo;
+    char name[24];
+    char description[80];
+    char expdate[10];
+} Estoques;
+
 void Menu();
 int LerArquivo();
 void RegistrarEstoque();
-void GravandoEstoque();
-void Simular();
+void GravandoEstoque(Estoques estoque);
+void Simul();
 void SimulVenda();
 void SimulCompra();
 // Declarando as funções de antemão para maior performance do código.
@@ -56,51 +64,175 @@ void Menu(){ // Menu de inicialização do programa.
 /* Avalia o preço de um único estoque e do estoque ao todo para o usuário
 ver as possibilidades em seu negócio, realizando tipos de avaliações*/
 
-void RegistrarEstoque(){ // Função de registro de estoque
-    for(codigo = 0; codigo <= 3; codigo++){
-        printf("Qual é o codigo do produto?: \n");
-        scanf("%i", &codigo);
-        printf("Qual e o nome do estoque?: \n");
-        scanf(" %[^\n]", name);
-        printf("Qual é a descricao do estoque?: \n");
-        scanf(" %[^\n]", description);
-        printf("Qual e o valor do estoque inicial?: \n");
-        scanf("%i", &stockstorage);
-        printf("Qual e a data de vencimento? (DD/MM/YYYY): \n");
-        scanf("%s", &expdate);
-        GravandoEstoque();
+void RegistrarEstoque() {
+    Estoques estoque;
+
+    for (int i = 0; i < 3; i++) {
+        printf("Qual é o código do produto?: \n");
+        scanf("%d", &estoque.codigo);
+
+        printf("Qual é o nome do estoque?: \n");
+        scanf(" %[^\n]", estoque.name);
+
+        printf("Qual é a descrição do estoque?: \n");
+        scanf(" %[^\n]", estoque.description);
+
+        printf("Qual é o valor do estoque inicial?: \n");
+        scanf("%d", &estoque.stockstorage);
+
+        printf("Qual é a data de vencimento? (DD/MM/YYYY): \n");
+        scanf("%s", estoque.expdate);
+
+        GravandoEstoque(estoque);
     }
     system("cls");
     Menu();
 }
+
 //alterar quantidade aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-void GravandoEstoque(){
-    FILE *ListaEstoques = fopen("ListaEstoques.txt","a+");
-	fprintf(ListaEstoques,"|==================================|\n");
-	fprintf(ListaEstoques,"|Nome:.........%s|\n",name);
-    fprintf(ListaEstoques,"|Codigo:.........00%i|\n",codigo);
-	fprintf(ListaEstoques,"|----------------------------------|\n");
-	fprintf(ListaEstoques,"|Quantidade:....%i |\n",stockstorage);
-    fprintf(ListaEstoques,"|Data de Venc:..%s |\n",expdate);
-	fprintf(ListaEstoques,"|Descrição:.....%s |\n",description);
-	fprintf(ListaEstoques,"|==================================|\n");
-    fprintf(ListaEstoques,"\n");
+void GravandoEstoque(Estoques estoque) {
+
+    FILE *ListaEstoques = fopen("ListaEstoques.txt", "a+");
+    if (ListaEstoques == NULL) {
+        printf("Erro ao abrir o arquivo para gravar.\n");
+        return;
+    }
+
+    fprintf(ListaEstoques, "|==================================|\n");
+    fprintf(ListaEstoques, "|Nome:.........%s|\n", estoque.name);
+    fprintf(ListaEstoques, "|Codigo:......%03d|\n", estoque.codigo);
+    fprintf(ListaEstoques, "|----------------------------------|\n");
+    fprintf(ListaEstoques, "|Quantidade:....%d|\n", estoque.stockstorage);
+    fprintf(ListaEstoques, "|Data de Venc:..%s|\n", estoque.expdate);
+    fprintf(ListaEstoques, "|Descrição:.....%s|\n", estoque.description);
+    fprintf(ListaEstoques, "|==================================|\n");
+    fprintf(ListaEstoques, "\n");
+
     fclose(ListaEstoques);
 }
 
-void SimulVenda(){
-    printf("Digite o código do produto que deseja vender: ");
-    scanf("%d", &codigo);
-    printf("Digite a quantidade vendida: ");
+
+void SimulVenda() {
+    FILE *temp;
+    char codBusca[10], linha[1024];
+    int codInt, qtdTemp;
+    bool encontrou = false;
+
+    ListaEstoques = fopen("ListaEstoques.txt", "r");
+    if (ListaEstoques == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        return;
+    }
+
+    temp = fopen("temp.txt", "w");
+    if (temp == NULL) {
+        printf("Erro ao criar o arquivo temporário.\n");
+        fclose(ListaEstoques);
+        return;
+    }
+
+    printf("Digite o código do produto (ex.: 001): ");
+    scanf("%s", codBusca);
+
+    printf("Digite a quantidade que deseja vender: ");
     scanf("%d", &stockunits);
+
+    while (fgets(linha, sizeof(linha), ListaEstoques) != NULL) {
+        if (strstr(linha, "|Codigo:") != NULL) {
+            sscanf(linha, "|Codigo:......%d|", &codInt);
+            if (codInt == atoi(codBusca)) {
+                encontrou = true;
+            }
+        }
+
+        if (encontrou && strstr(linha, "|Quantidade:") != NULL) {
+            sscanf(linha, "|Quantidade:....%d|", &qtdTemp);
+            qtdTemp -= stockunits;
+            if (qtdTemp < 0) qtdTemp = 0;
+            fprintf(temp, "|Quantidade:....%d|\n", qtdTemp);
+            encontrou = false;
+        } else {
+            fprintf(temp, "%s", linha);
+        }
+    }
+
+    fclose(ListaEstoques);
+    fclose(temp);
+
+    remove("ListaEstoques.txt");
+    rename("temp.txt", "ListaEstoques.txt");
+
+    printf("Venda concluída e estoque atualizado!\n");
+
+    do {
+        printf("Digite 5 para retornar ao menu.\n");
+        scanf("%i", &opSimul);
+    } while (opSimul != 5);
+
+    system("cls");
+    Menu();
 }
 
-void SimulCompra(){
-    printf("Digite o código do produto que deseja comprar: ");
-    scanf("%d", &codigo);
-    printf("Digite a quantidade que vai ser comprada: ");
+void SimulCompra() {
+    FILE *temp;
+    char codBusca[10], linha[1024];
+    int codInt, qtdTemp;
+    bool encontrou = false;
+
+    ListaEstoques = fopen("ListaEstoques.txt", "r");
+    if (ListaEstoques == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        return;
+    }
+
+    temp = fopen("temp.txt", "w");
+    if (temp == NULL) {
+        printf("Erro ao criar o arquivo temporário.\n");
+        fclose(ListaEstoques);
+        return;
+    }
+
+    printf("Digite o código do produto (ex.: 001): ");
+    scanf("%s", codBusca);
+
+    printf("Digite a quantidade que deseja adicionar: ");
     scanf("%d", &stockunits);
+
+    while (fgets(linha, sizeof(linha), ListaEstoques) != NULL) {
+        if (strstr(linha, "|Codigo:") != NULL) {
+            sscanf(linha, "|Codigo:......%d|", &codInt);
+            if (codInt == atoi(codBusca)) {
+                encontrou = true;
+            }
+        }
+
+        if (encontrou && strstr(linha, "|Quantidade:") != NULL) {
+            sscanf(linha, "|Quantidade:....%d|", &qtdTemp);
+            qtdTemp += stockunits;
+            fprintf(temp, "|Quantidade:....%d|\n", qtdTemp);
+            encontrou = false;
+        } else {
+            fprintf(temp, "%s", linha);
+        }
+    }
+
+    fclose(ListaEstoques);
+    fclose(temp);
+
+    remove("ListaEstoques.txt");
+    rename("temp.txt", "ListaEstoques.txt");
+
+    printf("Compra concluída e estoque atualizado!\n");
+
+    do {
+        printf("Digite 5 para retornar ao menu.\n");
+        scanf("%i", &opSimul);
+    } while (opSimul != 5);
+
+    system("cls");
+    Menu();
 }
+
 
 int main(){
     setlocale(LC_ALL, "Portuguese"); // Coloca o idioma em português.
